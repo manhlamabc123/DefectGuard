2fb7403c145573e63a2a67119687ce6f91ac0037
Jirachiii <trangdy1112@gmail.com>
2023-09-06 13:32:20 +0700

    Changing script

--- deepjit/torch-archive.sh
+++ deepjit/torch-archive.sh
@@ -6,3 +6,5 @@ torch-model-archiver --model-name deepjit \
     --serialized-file deepjit.pt \
     --handler handler.py \
     --extra-files deepjit.json,platform_dict.pkl
+
+mv deepjit.mar ../torchserve/model-store
